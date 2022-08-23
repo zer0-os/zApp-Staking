aa75fc0c6aa647cd102c05d9d45f0c43a4356249
Brett <colbr243@gmail.com>
2022-08-24 01:35:34 +1200

    fix: add missing files (#2)
    Webstorm git can be a pain sometimes!

---
+++ src/features/claim-pool-rewards/ClaimModal.tsx
@@ -0,0 +1,26 @@
+/*
+ * Wraps the claim modal for easier use in other sibling components.
+ */
+
+import { FC } from 'react';
+
+import { PoolInfo } from '../../lib/types/pool';
+import { BasicModalProps } from '../../lib/types/ui';
+
+import ClaimForm from './ClaimForm';
+import Modal from '@zero-tech/zui/components/Modal';
+import styles from './ClaimModal.module.scss';
+
+export interface ClaimModalProps extends PoolInfo, BasicModalProps {}
+
+const ClaimModal: FC<ClaimModalProps> = ({
+	poolInstance,
+	poolMetadata,
+	...modalProps
+}) => (
+	<Modal {...modalProps} className={styles.Container}>
+		<ClaimForm poolInstance={poolInstance} poolMetadata={poolMetadata} />
+	</Modal>
+);
+
+export default ClaimModal;
--- src/features/claim-pool-rewards/index.ts
+++ src/features/claim-pool-rewards/index.ts
@@ -1,2 +1,2 @@
-export { default as ClaimModal } from './ClaimModal';
-export type { ClaimModalProps } from './ClaimModal';
+export { default as ClaimModal } from './ClaimForm';
+export type { ClaimModalProps } from './ClaimForm';
