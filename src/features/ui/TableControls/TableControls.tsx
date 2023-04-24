87966a11197b3e60161777dbba1817a6a562ce6d
DomW <domw30@gmail.com>
2023-04-24 11:29:38 +0100

    refactor: add resize observer and controls for switching table view (#18)
    * fix: resolve missing key console error
    
    * fix: resolve console css warning
    
    * fix: center Connect Wallet wrapper
    
    * fix: revert connect wrapper style change
    
    * refactor: use resize observer to switch pool table view
    
    * refactor: update incorrect file in comments
    
    * refactor: use resize observer to switch table view and add toggle buttons
    
    * refactor: extract table controls component into ui

---
+++ src/features/ui/TableControls/TableControls.tsx
@@ -0,0 +1,23 @@
+import styles from './TableControls.module.scss';
+
+import { Controls, View, ViewToggle } from '@zero-tech/zui/components';
+
+interface TableControlsProps {
+	view: View;
+	onChangeView: (view: View) => void;
+}
+
+export const TableControls = ({
+	view,
+	onChangeView,
+}: TableControlsProps): JSX.Element => {
+	return (
+		<Controls>
+			<ViewToggle
+				className={styles.Toggle}
+				view={view}
+				onChange={onChangeView}
+			/>
+		</Controls>
+	);
+};
--- src/features/view-pools/PoolTable.tsx
+++ src/features/view-pools/PoolTable.tsx
@@ -1,31 +1,53 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
 import { COLUMNS } from './Pools.helpers';
 import { usePools } from '../../lib/hooks/usePools';
 
 import { PoolRow } from './PoolRow';
 import { PoolCard } from './PoolCard';
+import { TableControls } from '../ui/TableControls';
 import {
 	Body,
 	Grid,
 	Header,
 	HeaderGroup,
 	Table,
+	View,
 } from '@zero-tech/zui/components/Table';
 
 import styles from './PoolTable.module.scss';
 
-export const PoolTable: FC = () => {
-	const { wildPool, liquidityPool } = usePools();
+// @note: this value is being used in TableControls.module.scss - change in both places
+const GRID_WIDTH_TOGGLE = 600;
 
+export const PoolTable: FC = () => {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const { wildPool, liquidityPool } = usePools();
 	const tableData = [wildPool, liquidityPool];
+	const [view, setView] = useState<View>(View.TABLE);
+
+	useEffect(() => {
+		const resizeObserver = new ResizeObserver(() => {
+			if (containerRef.current) {
+				if (containerRef.current.offsetWidth <= GRID_WIDTH_TOGGLE) {
+					setView(View.GRID);
+				}
+			}
+		});
+		resizeObserver.observe(containerRef.current);
+		return () => resizeObserver.disconnect();
+	}, [containerRef]);
 
 	return (
-		<PoolView
-			poolAddressCollection={tableData.map(
-				(poolAddressCollection) => poolAddressCollection.address,
-			)}
-		/>
+		<div ref={containerRef}>
+			<TableControls view={view} onChangeView={setView} />
+			<PoolView
+				isGridView={view === View.GRID}
+				poolAddressCollection={tableData.map(
+					(poolAddressCollection) => poolAddressCollection.address,
+				)}
+			/>
+		</div>
 	);
 };
 
@@ -33,38 +55,47 @@ export const PoolTable: FC = () => {
  * PoolView Row/Card  *
  *********************/
 interface PoolViewProps {
+	isGridView: boolean;
 	poolAddressCollection: string[];
 }
 
-const PoolView = ({ poolAddressCollection }: PoolViewProps) => {
+const PoolView = ({ isGridView, poolAddressCollection }: PoolViewProps) => {
 	if (poolAddressCollection.length === 0) {
 		return <></>;
 	}
-
-	return (
-		<div className={styles.PoolView}>
-			<Grid className={styles.Grid}>
-				{poolAddressCollection?.map((poolAddress) => (
-					<PoolCard poolAddress={poolAddress} />
-				))}
-			</Grid>
-
-			<div className={styles.Table}>
-				<Table>
-					<HeaderGroup>
-						{COLUMNS.map((column) => (
-							<Header key={column.id} alignment={column.alignment}>
-								{column.header}
-							</Header>
-						))}
-					</HeaderGroup>
-					<Body>
-						{poolAddressCollection?.map((poolAddress) => (
-							<PoolRow poolAddress={poolAddress} />
-						))}
-					</Body>
-				</Table>
+	if (isGridView) {
+		return (
+			<div className={styles.PoolView}>
+				<Grid className={styles.Grid}>
+					{poolAddressCollection?.map((poolAddress) => (
+						<PoolCard key={`pool-${poolAddress}`} poolAddress={poolAddress} />
+					))}
+				</Grid>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div className={styles.PoolView}>
+				<div className={styles.Table}>
+					<Table>
+						<HeaderGroup>
+							{COLUMNS.map((column) => (
+								<Header key={column.id} alignment={column.alignment}>
+									{column.header}
+								</Header>
+							))}
+						</HeaderGroup>
+						<Body>
+							{poolAddressCollection?.map((poolAddress) => (
+								<PoolRow
+									key={`pool-${poolAddress}`}
+									poolAddress={poolAddress}
+								/>
+							))}
+						</Body>
+					</Table>
+				</div>
+			</div>
+		);
+	}
 };
