effbc84b138d87c02b890c8342ec4b6099bf0ab4
dominic <domw30@gmail.com>
2023-04-24 13:46:56 +1000

    refactor: use resize observer to switch table view and add toggle buttons

--- src/features/my-deposits/DepositsTable.tsx
+++ src/features/my-deposits/DepositsTable.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { COLUMNS } from './Deposits.helpers';
@@ -6,17 +6,23 @@ import { ROUTE_NAMES, ROUTES } from '../../lib/constants/routes';
 import { useAllDeposits, DepositData } from '../../lib/hooks/useAllDeposits';
 
 import { DepositRow } from './DepositRow';
+import { DepositCard } from './DepositCard';
 
 import {
 	Body,
+	Controls,
 	Grid,
 	Header,
 	HeaderGroup,
 	Table,
+	View,
+	ViewToggle,
 } from '@zero-tech/zui/components/Table';
 
 import styles from './DepositsTable.module.scss';
-import { DepositCard } from './DepositCard';
+
+// @note: this value is being used in DepositsTable.module.scss - change in both places
+const GRID_WIDTH_TOGGLE = 600;
 
 export interface DepositsTableProps {
 	/*
@@ -27,13 +33,32 @@ export interface DepositsTableProps {
 }
 
 export const DepositsTable: FC<DepositsTableProps> = ({ account }) => {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const [view, setView] = useState<View>(View.TABLE);
+
 	const { data: queryData, isLoading } = useAllDeposits({ account });
 
+	useEffect(() => {
+		const resizeObserver = new ResizeObserver(() => {
+			if (containerRef.current) {
+				if (containerRef.current.offsetWidth <= GRID_WIDTH_TOGGLE) {
+					setView(View.GRID);
+				}
+			}
+		});
+		resizeObserver.observe(containerRef.current);
+		return () => resizeObserver.disconnect();
+	}, [containerRef]);
+
 	return (
-		<DepositsView
-			depositsCollection={queryData?.deposits}
-			isLoading={isLoading}
-		/>
+		<div ref={containerRef}>
+			<DepositsTableControls view={view} onChangeView={setView} />
+			<DepositsView
+				depositsCollection={queryData?.deposits}
+				isLoading={isLoading}
+				isGridView={view === View.GRID}
+			/>
+		</div>
 	);
 };
 
@@ -43,37 +68,48 @@ export const DepositsTable: FC<DepositsTableProps> = ({ account }) => {
 interface DepositsViewProps {
 	depositsCollection: DepositData[];
 	isLoading: boolean;
+	isGridView: boolean;
 }
 
-const DepositsView = ({ depositsCollection, isLoading }: DepositsViewProps) => {
+const DepositsView = ({
+	depositsCollection,
+	isLoading,
+	isGridView,
+}: DepositsViewProps) => {
 	//  If loading, or there are already deposits loaded, render the table
 	if (isLoading || Boolean(depositsCollection?.length)) {
-		return (
-			<div className={styles.DepositsView}>
-				<Grid className={styles.Grid}>
-					{depositsCollection?.map((deposit) => (
-						<DepositCard deposit={deposit} isLoading={isLoading} />
-					))}
-				</Grid>
-
-				<div className={styles.Table}>
-					<Table>
-						<HeaderGroup>
-							{COLUMNS.map((column) => (
-								<Header key={column.id} alignment={column.alignment}>
-									{column.header}
-								</Header>
-							))}
-						</HeaderGroup>
-						<Body>
-							{depositsCollection?.map((deposit) => (
-								<DepositRow deposit={deposit} />
-							))}
-						</Body>
-					</Table>
+		if (isGridView) {
+			return (
+				<div className={styles.DepositsView}>
+					<Grid className={styles.Grid}>
+						{depositsCollection?.map((deposit) => (
+							<DepositCard deposit={deposit} isLoading={isLoading} />
+						))}
+					</Grid>
 				</div>
-			</div>
-		);
+			);
+		} else {
+			return (
+				<div className={styles.DepositsView}>
+					<div className={styles.Table}>
+						<Table>
+							<HeaderGroup>
+								{COLUMNS.map((column) => (
+									<Header key={column.id} alignment={column.alignment}>
+										{column.header}
+									</Header>
+								))}
+							</HeaderGroup>
+							<Body>
+								{depositsCollection?.map((deposit) => (
+									<DepositRow deposit={deposit} />
+								))}
+							</Body>
+						</Table>
+					</div>
+				</div>
+			);
+		}
 	}
 
 	// If loaded, and there are no deposits, render a message
@@ -105,4 +141,28 @@ interface MessageProps {
 
 const Message = ({ children }: MessageProps) => {
 	return <p className={styles.Empty}>{children}</p>;
+};
+
+/**********************
+ * Controls
+ *********************/
+
+interface DepositsTableControlsProps {
+	view: View;
+	onChangeView: (view: View) => void;
+}
+
+const DepositsTableControls = ({
+	view,
+	onChangeView,
+}: DepositsTableControlsProps): JSX.Element => {
+	return (
+		<Controls>
+			<ViewToggle
+				className={styles.Toggle}
+				view={view}
+				onChange={onChangeView}
+			/>
+		</Controls>
+	);
 };
