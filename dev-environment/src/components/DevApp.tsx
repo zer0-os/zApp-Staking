b3f1f2bc72ef638240d95926ecee0c486971b98e
Brett <12437916+colbr@users.noreply.github.com>
2023-08-21 17:02:28 +1200

    chore: add container app

---
+++ dev-environment/src/components/DevApp.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { StakingZApp } from '@/index';
+
+import { ethers } from 'ethers';
+import { useAccount, useConnect } from 'wagmi';
+import { useEthersProvider } from '../lib/useEthersProvider';
+import { injectedConnector } from '../lib/connectors';
+
+import { DevControls } from './DevControls';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const RPC_URL = import.meta.env.VITE_RPC_URL_1;
+
+export const DevApp = () => {
+	const { address } = useAccount();
+	const { connect } = useConnect({
+		connector: injectedConnector,
+	});
+
+	const provider = useEthersProvider({ chainId: 1 });
+
+	if (!RPC_URL) {
+		throw new Error(
+			'Fill out .env.development.local before running dev environment',
+		);
+	}
+
+	return (
+		<>
+			<DevControls />
+			<StakingZApp
+				provider={provider ?? new ethers.providers.JsonRpcProvider(RPC_URL)}
+				route={'wilder'}
+				web3={{
+					chainId: provider?.network.chainId ?? 1,
+					address: address,
+					connectWallet: connect,
+				}}
+			/>
+		</>
+	);
+};
