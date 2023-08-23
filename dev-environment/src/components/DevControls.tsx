6008649d86a5bb9779ba70b28ab2b9664756d933
Brett <colbr243@gmail.com>
2023-08-23 12:36:37 +1200

    chore: container app (#32)
    * chore: add container app
    
    * chore: some config

---
+++ dev-environment/src/components/DevControls.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { useAccount, useConnect } from 'wagmi';
+import { truncateAddress } from '@zero-tech/zui/utils';
+import { injectedConnector } from '../lib/connectors';
+import { version } from '../../../package.json';
+
+export const DevControls = () => {
+	const { address } = useAccount();
+	const { connect } = useConnect({
+		connector: injectedConnector,
+	});
+
+	const content = address ? (
+		<span>
+			Connected as <b>{truncateAddress(address)}</b>
+		</span>
+	) : (
+		<button onClick={() => connect()}>Connect</button>
+	);
+
+	return (
+		<header
+			style={{
+				position: 'fixed',
+				bottom: 0,
+				width: '100%',
+				boxSizing: 'border-box',
+				background: 'white',
+				padding: '0.5rem',
+				borderRadius: '0.5rem',
+				display: 'flex',
+				justifyContent: 'space-between',
+				height: '2rem',
+				alignItems: 'center',
+				color: 'black',
+			}}
+		>
+			<b>zApp Staking {version}</b>
+			<span>{content}</span>
+		</header>
+	);
+};
