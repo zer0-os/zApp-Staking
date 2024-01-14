e4aff082a2a823ead6599dfb109cef4367801c7b
Brett <colbr243@gmail.com>
2024-01-14 22:51:50 +0000

    build: erc20 abi json -> ts (#42)

--- src/lib/contracts/wild-token.ts
+++ src/lib/contracts/wild-token.ts
@@ -1,10 +1,10 @@
 import { Contract } from 'ethers';
-import ERC20Abi from './ERC20.json';
+import { ERC20_ABI } from './erc20';
 
 export const wildTokenContract = (provider: any) => {
 	return new Contract(
 		'0x2a3bFF78B79A009976EeA096a51A948a3dC00e34',
-		ERC20Abi as any[],
+		ERC20_ABI,
 		provider,
 	);
 };
