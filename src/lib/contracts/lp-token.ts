a3aa71968c9b2b45404054cc5ce3ea620c073612
Brett <12437916+colbr@users.noreply.github.com>
2024-01-15 11:50:47 +1300

    build: erc20 abi json -> ts

--- src/lib/contracts/lp-token.ts
+++ src/lib/contracts/lp-token.ts
@@ -1,10 +1,10 @@
 import { Contract } from 'ethers';
-import ERC20Abi from './ERC20.json';
+import { ERC20_ABI } from './erc20';
 
 export const lpTokenContract = (provider: any) => {
 	return new Contract(
 		'0xcaA004418eB42cdf00cB057b7C9E28f0FfD840a5',
-		ERC20Abi as any[],
+		ERC20_ABI,
 		provider,
 	);
 };
--- src/lib/contracts/weth-token.ts
+++ src/lib/contracts/weth-token.ts
@@ -1,10 +1,10 @@
 import { Contract } from 'ethers';
-import ERC20Abi from './ERC20.json';
+import { ERC20_ABI } from './erc20';
 
 export const wethTokenContract = (provider: any) => {
 	return new Contract(
 		'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-		ERC20Abi as any[],
+		ERC20_ABI,
 		provider,
 	);
 };
